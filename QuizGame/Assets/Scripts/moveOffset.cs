2120567e1b3e9718179ad412cb812870ff1a0b16
Arley Novais <nascimentoarle@gmail.com>
2020-09-05 18:18:26 -0400

    Add inicial screen

---
+++ QuizGame/Assets/Scripts/moveOffset.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moveOffset : MonoBehaviour {
+
+	private Material material;
+	public float velocidadeX, velocidadeY;
+	public float incremento;
+	private float offset;
+
+
+	// Use this for initialization
+	void Start () {
+		material = GetComponent<Renderer> ().material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		offset += incremento;
+		material.SetTextureOffset ("_MainTex", 
+			new Vector2(offset * velocidadeX, offset * velocidadeY));
+
+	}
+}
