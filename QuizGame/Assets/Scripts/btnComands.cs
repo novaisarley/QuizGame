b7942f16415b46f874c5fa4b65f396ff187a754d
Arley Novais <nascimentoarle@gmail.com>
2020-09-05 21:26:31 -0400

    Add initial screen bt listeners

---
+++ QuizGame/Assets/Scripts/btnComands.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class btnComands : MonoBehaviour {
+
+	public void irParaCena(string nomeCena){
+		SceneManager.LoadScene (nomeCena);
+		//SceneManager.LoadScene(nomeCena, LoadSceneMode.Additive);
+	}
+
+	public void finish(){
+		Application.Quit ();
+	}
+}
