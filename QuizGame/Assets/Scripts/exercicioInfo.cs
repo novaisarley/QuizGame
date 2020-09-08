7c023f9cd0f00c6915d6011fe17330e32f778217
Arley Novais <nascimentoarle@gmail.com>
2020-09-08 09:54:22 -0400

    Add exercicio script

---
+++ QuizGame/Assets/Scripts/exercicioInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class exercicioInfo : MonoBehaviour {
+
+	public int idExercicio;
+	public string nomeExercicio;
+	public int minPontos1Estrela;
+	public int minPontos2Estrelas;
+
+	public Text txtIdExercicio;
+	public GameObject[] estrelas;
+
+
+	// Use this for initialization
+	void Start () {
+		txtIdExercicio.text = idExercicio.ToString();
+		preencherEstrelas ();
+	}
+
+	void preencherEstrelas(){
+
+		int nEstrelas = 0;
+
+		foreach (GameObject e in estrelas) {
+			e.SetActive (false);
+		}
+		
+	}
+}
